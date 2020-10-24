572b3d6905e1913ae56dc6071063ebab1e43e3b0
Aniket Sunil Pawar <54832294+aniketpawar9211@users.noreply.github.com>
2020-10-24 09:49:51 +0530

    One Line code for Even and odd simple

--- assi_Q2_even_or_odd.py
+++ assi_Q2_even_or_odd.py
@@ -1,7 +1,3 @@
-no=int(input("Enter Number To Cheack Even or Odd:---"))
-if(no%2==0):
-    print("Given No is {}".format("Even"))
-else:
-    print("Given No is {}".format("Odd"))
+print(['even','odd'][int(input("Enter No:-"))%2])
 
  
